38129f96415a4b89e46cd4a1e40041fbbd38e7e2
Neil Griffin <neil.griffin.scm@gmail.com>
2019-02-26 10:57:21 -0500

    Add the applicant-mvcbean-spring-thymeleaf-portlet

---
+++ maven/apps/applicant/applicant-mvcbean-spring-thymeleaf-portlet/src/main/java/com/liferay/blade/samples/applicant/mvcbean/spring/thymeleaf/portlet/ApplicantPortlet.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-2018 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.blade.samples.applicant.mvcbean.spring.thymeleaf.portlet;
+
+import java.io.IOException;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.portlet.HeaderPortlet;
+import javax.portlet.HeaderRequest;
+import javax.portlet.HeaderResponse;
+import javax.portlet.PortletException;
+import javax.portlet.annotations.InitParameter;
+import javax.portlet.annotations.PortletConfiguration;
+import javax.portlet.annotations.Preference;
+import javax.portlet.annotations.Supports;
+
+import com.liferay.bean.portlet.LiferayPortletConfiguration;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@ApplicationScoped
+@PortletConfiguration(
+	portletName = "portlet1", prefs = { @Preference(name = "datePattern", values = { "MM/dd/yyyy" }) },
+	supports = { @Supports(portletModes = { "view", "edit", "help" }) }, resourceBundle = "content.portlet1"
+)
+@LiferayPortletConfiguration(
+	portletName = "portlet1", properties = { "com.liferay.portlet.display-category=category.sample" }
+)
+public class ApplicantPortlet implements HeaderPortlet {
+
+	@Override
+	public void renderHeaders(HeaderRequest headerRequest, HeaderResponse headerResponse) throws PortletException,
+		IOException {
+
+		boolean liferayDetected = PortletContainer.LIFERAY.isDetected(headerRequest);
+		boolean plutoDetected = PortletContainer.PLUTO.isDetected(headerRequest);
+
+		if (plutoDetected) {
+			headerResponse.addDependency("jQuery", "com.jquery", "1.12.1",
+				"<script src=\"https://code.jquery.com/jquery-1.12.4.js\"></script>");
+		}
+
+		if (liferayDetected || plutoDetected) {
+			headerResponse.addDependency("jQueryUI", "com.jquery", "1.12.1",
+				"<script src=\"https://code.jquery.com/ui/1.12.1/jquery-ui.js\"></script>");
+			headerResponse.addDependency("jQueryUICSS", "com.jquery", "1.12.1",
+				"<link rel=\"stylesheet\" href=\"//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css\">");
+		}
+	}
+}
---
+++ maven/apps/applicant/applicant-mvcbean-spring-thymeleaf-portlet/src/main/java/com/liferay/blade/samples/applicant/mvcbean/spring/thymeleaf/templating/ApplicantVariableValidator.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-2018 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.blade.samples.applicant.mvcbean.spring.thymeleaf.templating;
+
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletContext;
+
+import org.apache.pluto.thymeleaf.mvc.portlet.spring.SpringVariableValidator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Primary;
+
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@Component
+@Primary
+public class ApplicantVariableValidator extends SpringVariableValidator {
+
+	@Autowired
+	private PortletConfig portletConfig;
+
+	@Autowired
+	private PortletContext portletContext;
+
+	public ApplicantVariableValidator() {
+		super(null, null);
+	}
+
+	@Override
+	public PortletConfig getPortletConfig() {
+		return portletConfig;
+	}
+
+	@Override
+	public PortletContext getPortletContext() {
+		return portletContext;
+	}
+
+	@Override
+	public boolean isValidName(String name, boolean headerPhase, boolean renderPhase, boolean resourcePhase) {
+
+		boolean valid = super.isValidName(name, headerPhase, renderPhase, resourcePhase);
+
+		if (!valid) {
+			return false;
+		}
+
+		// In order to improve Thymeleaf performance, prevent the following beans from being included in EL resolution.
+		if (name.equals("applicantPortlet") || name.equals("attachmentManager") || name.contains("cityService") ||
+				name.endsWith("Controller") || name.equals("mvcEventListener") || name.contains("provinceService")) {
+			return false;
+		}
+
+		return true;
+	}
+}
